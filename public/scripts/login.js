51acc7098c4da6bf65bd6024a11304826aa4d849
UhAhUhAhAhGg <149268931+UhAhUhAhAhGg@users.noreply.github.com>
2024-04-27 11:31:53 -0400

    Vista del inicio antes de iniciar sesión y vista de gerente falta vista inicio del cajero

---
+++ public/scripts/login.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var botonLogin = document.getElementById('botonLogin');
+    botonLogin.addEventListener('click', function() {
+      alert('Aquí iría la lógica para iniciar sesión');
+    });
+  });
+  
